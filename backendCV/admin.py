4721c7ed84ca4b9eef9db0a112abe6678508b802
CarlosFarfan29 <cfarfana@unac.edu.pe>
2023-11-27 10:54:21 -0500

    Add: Models to Admin

--- backendCV/admin.py
+++ backendCV/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from backendCV.models import User, Core, Department, Position
+from backendCV.models import User, Core, Department, Position, Client, Invoice, Sale
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Position)
 admin.site.register(Core)
 admin.site.register(Department)
+admin.site.register(Client)
+admin.site.register(Invoice)
+admin.site.register(Sale)
